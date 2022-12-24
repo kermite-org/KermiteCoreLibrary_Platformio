626eda360847752261a96aa818d66378d653a3df
yahiro <yahiro07@users.noreply.github.com>
2022-12-24 13:47:52 +0900

    write blink

--- development_app1/src/main.cpp
+++ development_app1/src/main.cpp
@@ -1,9 +1,17 @@
 #include <Arduino.h>
 
+// tested on Tiny2040
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(20, OUTPUT);
+  digitalWrite(20, HIGH);
+  pinMode(19, OUTPUT);
+  digitalWrite(19, HIGH);
+  pinMode(18, OUTPUT);
+  digitalWrite(18, HIGH);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(20, !digitalRead(20));
+  delay(100);
 }
