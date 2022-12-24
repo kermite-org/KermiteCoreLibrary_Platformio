0b51f08a03e66a3e4f54adf0ededdcfedf831774
yahiro <yahiro07@users.noreply.github.com>
2022-12-24 16:23:07 +0900

    emit HID keycode on key press

--- development_app1/src/main.cpp
+++ development_app1/src/main.cpp
@@ -1,3 +1,4 @@
+#include <Adafruit_TinyUSB.h>
 #include <Arduino.h>
 
 // tested on Tiny2040
@@ -21,19 +22,46 @@ static void initBoardButton() { pinMode(7, INPUT_PULLUP); }
 
 static bool readBoardButton() { return digitalRead(7) == LOW; }
 
+extern Adafruit_USBD_Device USBDevice;
+
+static const uint8_t desc_hid_report[] = {TUD_HID_REPORT_DESC_KEYBOARD()};
+
+static Adafruit_USBD_HID usb_hid;
+
 void setup() {
   initBoardLEDs();
   initBoardButton();
+
+  usb_hid.setPollInterval(2);
+  usb_hid.setReportDescriptor(desc_hid_report, sizeof(desc_hid_report));
+
+  usb_hid.begin();
+  writeBoardLED(0, true);
+  while (!USBDevice.mounted())
+    delay(1);
+  writeBoardLED(0, false);
 }
 
-void updateButton() {
+static void sendHidKey(char chr, bool pressed) {
+  if (usb_hid.ready()) {
+    if (pressed) {
+      usb_hid.keyboardPress(0, chr);
+    } else {
+      usb_hid.keyboardRelease(0);
+    }
+  }
+}
+
+static void updateButton() {
   static bool pressed = false;
   bool nextPressed = readBoardButton();
   if (!pressed && nextPressed) {
     Serial.printf("pressed\n");
+    sendHidKey('a', true);
   }
   if (pressed && !nextPressed) {
     Serial.printf("released\n");
+    sendHidKey('a', false);
   }
   pressed = nextPressed;
   writeBoardLED(1, pressed);
