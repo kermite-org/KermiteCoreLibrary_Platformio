9132a76b3112522a5114d5f4eafe78f61b0c67d3
yahiro <yahiro07@users.noreply.github.com>
2022-12-26 16:26:29 +0900

    update node script

--- development_app1_tinyusb/misc/node_hid_testapp/index.js
+++ development_app1_tinyusb/misc/node_hid_testapp/index.js
@@ -1,24 +1,37 @@
 const HID = require("node-hid");
-const devices = HID.devices();
-const deviceInfo = devices.find(
-  (d) => d.vendorId === 0xf055 && d.productId === 0xa57a
-);
-if (deviceInfo) {
-  const device = new HID.HID(deviceInfo.path);
 
-  const onData = (data) => {
-    const bytes = new Uint8Array(data);
-    console.log("received");
-    console.log({ bytes });
-  };
+function start() {
+  const devices = HID.devices();
+  console.log({ devices });
+  const deviceInfo = devices.find(
+    (d) =>
+      d.vendorId === 0xf055 &&
+      d.productId === 0xa57a &&
+      d.usagePage === 65280 &&
+      d.usage === 1
+  );
+  console.log({ deviceInfo });
+  if (deviceInfo) {
+    const device = new HID.HID(deviceInfo.path);
 
-  device.on("data", onData);
+    const onData = (data) => {
+      const bytes = new Uint8Array(data);
+      console.log("received");
+      console.log({ bytes });
+    };
 
-  console.log("sending...");
-  device.write([0x00, 0x01, 0x01, 0x05, 0xff, 0xff]);
+    device.on("data", onData);
 
-  process.on("SIGINT", function () {
-    device.close();
-    process.exit(0);
-  });
+    setTimeout(() => {
+      console.log("sending...");
+      device.write([0x04, 0x01, 0x01, 0x05, 0xff, 0xff]);
+    }, 1000);
+
+    process.on("SIGINT", function () {
+      device.close();
+      process.exit(0);
+    });
+  }
 }
+
+start();
