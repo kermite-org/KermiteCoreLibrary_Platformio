12d270b53038180e13514d6f6901c33293a9631a
yahiro <yahiro07@users.noreply.github.com>
2022-12-29 06:45:33 +0900

    change led pin

--- examples/development_app1_tinyusb/src/app4.cpp
+++ examples/development_app1_tinyusb/src/app4.cpp
@@ -1,15 +1,15 @@
 #include <Arduino.h>
 
-//Tiny2040, blink
+//blink, Tiny2040
 
-const int pinLED = 19;
+const int pinLED = 20;
 
 void app4Entry() {
   pinMode(pinLED, OUTPUT);
+  pinMode(19, OUTPUT);
+  digitalWrite(19, HIGH);
   pinMode(18, OUTPUT);
   digitalWrite(18, HIGH);
-  pinMode(20, OUTPUT);
-  digitalWrite(20, HIGH);
 
   while (1) {
     digitalWrite(pinLED, !digitalRead(pinLED));
