27e3185ea619c592b3fc46d7c83bec1cc6aee8d9
yahiro <yahiro07@users.noreply.github.com>
2023-01-03 07:31:03 +0900

    add dummy usb and flash implementation

---
+++ src/km1/infrastructure/arduino/flashPersistSector_dummy.cpp
@@ -0,0 +1,7 @@
+#if defined(KERMITECORE_USE_FLASH_PERSIST_SECTOR_DUMMY_IMPL)
+
+#include "../flashPersistSector.h"
+void flashPersistSector_read(uint8_t *bytes4096) {}
+void flashPersistSector_write(uint8_t *bytes4096) {}
+
+#endif
---
+++ src/km1/infrastructure/arduino/usbIoCore_dummy.cpp
@@ -0,0 +1,33 @@
+#if defined(KERMITECORE_USE_USB_DUMMY_IMPL)
+
+#include "../usbIoCore.h"
+
+void usbIoCore_initialize() {}
+
+void usbIoCore_hidKeyboard_writeReport(uint8_t *pReportBytes8) {}
+
+uint8_t usbIoCore_hidKeyboard_getStatusLedFlags() {
+  return 0;
+}
+
+void usbIoCore_hidMouse_writeReport(uint8_t *pReportBytes7) {}
+
+void usbIoCore_hidConsumerControl_writeReport(uint8_t *pReportBytes2) {}
+
+bool usbIoCore_rawHid_writeData(uint8_t *pDataBytes64) {
+  return true;
+}
+
+bool usbIoCore_rawHid_readDataIfExists(uint8_t *pDataBytes64) {
+  return true;
+}
+
+bool usbIoCore_isConnectedToHost() {
+  return true;
+}
+
+void usbIoCore_setProductName(const char *productNameText) {}
+
+void usbIoCore_setSerialNumber(const char *serialNumberText) {}
+
+#endif
