685c87cefc9f657104ee75d46c81f2060780a1d8
yahiro <yahiro07@users.noreply.github.com>
2023-01-08 19:34:19 +0900

    add base code for development

---
+++ development/testbed/src/app1.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+
+//this is a template source file to write a development application
+
+void app1Entry() {
+  while (1) {
+    yield();
+    delay(1);
+  }
+}
--- development/testbed/src/main.cpp
+++ development/testbed/src/main.cpp
@@ -2,9 +2,11 @@
 #include <Arduino.h>
 
 extern void appEntry();
+extern void app1Entry();
 
 void setup() {
   appEntry();
+  // app1Entry();
 }
 
 void loop() {}
