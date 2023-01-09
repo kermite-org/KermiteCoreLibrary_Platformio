9c598397097bc24c5927849d211f3f7c351fb6bb
yahiro <yahiro07@users.noreply.github.com>
2023-01-10 00:31:39 +0900

    support external spi flash

--- src/km1/infrastructure/arduino/flashPersistSector_adruino_adafruit_spiFlash.cpp
+++ src/km1/infrastructure/arduino/flashPersistSector_adruino_adafruit_spiFlash.cpp
@@ -11,6 +11,8 @@
 
 #if defined(ARDUINO_ARCH_RP2040)
 static Adafruit_FlashTransport_RP2040 flashTransport;
+#elif defined(KERMITECORE_FLASHSTORAGE_ADAFRUIT_SPIFLASH__QSPI_EXTERNAL_FLASH_DEVICE)
+Adafruit_FlashTransport_QSPI flashTransport;
 #endif
 
 static Adafruit_SPIFlash flash(&flashTransport);
@@ -18,10 +20,30 @@ static Adafruit_SPIFlash flash(&flashTransport);
 static const int persistDataSize = flashPersistSector_DataSize;
 static int32_t locationOffset = -1;
 
+#define P25Q16H                                                         \
+  {                                                                     \
+    .total_size = (1UL << 21), /* 2 MiB */                              \
+        .start_up_time_us = 5000, .manufacturer_id = 0x85,              \
+    .memory_type = 0x60, .capacity = 0x15, .max_clock_speed_mhz = 85,   \
+    .quad_enable_bit_mask = 0x02, .has_sector_protection = false,       \
+    .supports_fast_read = true, .supports_qspi = true,                  \
+    .supports_qspi_writes = true, .write_status_register_split = false, \
+    .single_status_byte = false, .is_fram = false,                      \
+  }
+
+static void beginFlash() {
+#ifdef KERMITECORE_FLASHSTORAGE_ADAFRUIT_SPIFLASH__QSPI_EXTERNAL_FLASH_DEVICE
+  static const SPIFlash_Device_t spiFlashDevice = KERMITECORE_FLASHSTORAGE_ADAFRUIT_SPIFLASH__QSPI_EXTERNAL_FLASH_DEVICE;
+  flash.begin(&spiFlashDevice);
+#else
+  flash.begin();
+#endif
+}
+
 void ensureInitialized() {
   static bool initialized = false;
   if (!initialized) {
-    flash.begin();
+    beginFlash();
     uint32_t fsSize = flash.size();
     if (fsSize >= persistDataSize) {
       locationOffset = fsSize - persistDataSize;
